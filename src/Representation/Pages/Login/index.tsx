3b6a32a5f7fab19f59faffbe376934bf0f555ed1
Neider Pandales <napandales@indracompany.com>
2023-02-01 17:25:03 -0500

    feat: feature/register-design--form se realiza el formulario de registro

---
+++ src/Representation/Pages/Login/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "../../Components/Button";
+import H1 from "../../Components/H1";
+import FormLogin from "../../Containers/FormLogin";
+import FormRegister from "../../Containers/FormRegister";
+import { classCss } from "../../styles";
+
+import "./styles.less";
+
+interface PropsType {
+  props: any
+}
+
+interface State {
+  form: "auth" | "register"
+}
+
+export default class Login extends React.Component<PropsType, State> {
+  constructor (props: any) {
+    super(props);
+    this.state = {
+      form: "auth"
+    };
+  }
+
+  handler = (campo: "auth" | "register"): void => {
+    this.setState({
+      form: campo
+    });
+  };
+
+  render (): React.ReactNode {
+    return <>
+    <H1/>
+    <section className="containerLogin">
+      <div className="containerLogin__buttons">
+        <Button
+        event={() => { this.handler("register"); }}
+        text="Registro"
+        clase={ this.state.form === "register" ? classCss.primary + " underline" : classCss.secundary}/>
+        <Button
+        event={() => { this.handler("auth"); }}
+        text="Ingreso"
+        clase={ this.state.form === "auth" ? classCss.primary + " underline" : classCss.secundary}/>
+      </div>
+      { this.state.form === "auth" ? <FormLogin/> : <FormRegister/>}
+    </section>
+    </>;
+  }
+}
