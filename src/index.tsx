c04072f119e407db152f28ffbfb9e5ccca817dbb
Neider Pandales <napandales@indracompany.com>
2023-02-01 11:49:17 -0500

    feat: se agregan las fuentes y se instalar git-commit-msg-linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.less';
-import Layout from './Representation/Containers/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Layout/>
-    <h1>Bienvenido a </h1>
+    <h1>Bienvenido a <br/>Omnishop</h1>
   </React.StrictMode>
 );
